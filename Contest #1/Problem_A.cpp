3d708b7a92c658718d298d6591ce2c2218a8ec21
Mo7amedAbady <mabady648@gmail.com>
2026-09-16 18:03:59 +0300

    Add Problem_A.cpp with basic output

---
+++ Contest #1/Problem_A.cpp
@@ -0,0 +1,13 @@
+// =============
+//     ABADY
+// =============
+
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define endl '\n'
+using namespace std;
+
+int main() {
+    fast;
+    cout << 67 << endl;
+}
