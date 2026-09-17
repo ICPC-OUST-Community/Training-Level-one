ee5fc1d42df2676f8dcb9c4240cfb1a4ef398263
Mo7amedAbady <mabady648@gmail.com>
2026-09-17 03:21:35 +0300

    Update Problem_C.cpp

--- Contest #1/Problem_C.cpp
+++ Contest #1/Problem_C.cpp
@@ -12,35 +12,18 @@ int main() {
     while (t--) {
         int n; cin >> n;
         int arr[n];
-        bool vis[n]{};
-        for (int i = 0; i < n; i++) cin >> arr[i];
-        for (int i = 0; i < n; i++) {
-            if (arr[i] % 6 == 0) {
-                vis[i] = 1;
-                cout << arr[i] << ' ';
-            }
+        for (int i=0;i<n;i++) cin >> arr[i];
+        vector<int> v6, v2, v3, v;
+        for (int i=0;i<n;i++){
+            if (arr[i] % 6 == 0) v6.push_back(arr[i]);
+            else if (arr[i] % 2 == 0) v2.push_back(arr[i]);
+            else if (arr[i] % 3 == 0) v3.push_back(arr[i]);
+            else v.push_back(arr[i]);
         }
-        for (int i = 0; i < n; i++) {
-            if (vis[i]) continue;
-            if (arr[i] % 2 == 0) {
-                vis[i] = 1;
-                cout << arr[i] << ' ';
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (vis[i]) continue;
-            if ((arr[i] % 6 != 0) && (arr[i] % 3 != 0) && (arr[i] % 2 != 0)) {
-                vis[i] = 1;
-                cout << arr[i] << ' ';
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (vis[i]) continue;
-            if (arr[i] % 3 == 0) {
-                vis[i] = 1;
-                cout << arr[i] << ' ';
-            }
-        }
+        for (auto I:v6) cout << I << ' ';
+        for (auto I:v2) cout << I << ' ';
+        for (auto I:v)  cout << I << ' ';
+        for (auto I:v3) cout << I << ' ';
         cout << endl;
     }
 }
